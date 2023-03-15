5583a795f0bc71dd900dc84443ac1d19b0466159
codertinie <martin.munyao@moringaschool.com>
2023-03-16 02:48:22 +0300

    🗃️: connected the database with the products

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,85 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../styles/ProductList.css";
 
 const ProductList = () => {
-    return (
-        <div>
-            <table>
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Category</th>
-      <th>Stock Amount</th>
-      <th>Price</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Coca Cola</td>
-      <td>300ml</td>
-      <td>120</td>
-      <td>$1.50</td>
-      <td>
-        <button class="view-btn">View</button>
-        <button class="edit-btn">Edit</button>
-      </td>
-    </tr>
-    <tr>
-      <td>Sprite</td>
-      <td>500ml</td>
-      <td>80</td>
-      <td>$2.00</td>
-      <td>
-        <button class="view-btn">View</button>
-        <button class="edit-btn">Edit</button>
-      </td>
-    </tr>
-    <tr>
-      <td>Fanta</td>
-      <td>1 liter</td>
-      <td>50</td>
-      <td>$3.50</td>
-      <td>
-        <button class="view-btn">View</button>
-        <button class="edit-btn">Edit</button>
-      </td>
-    </tr>
-    <tr>
-      <td>Pepsi</td>
-      <td>300ml</td>
-      <td>90</td>
-      <td>$1.50</td>
-      <td>
-        <button class="view-btn">View</button>
-        <button class="edit-btn">Edit</button>
-      </td>
-    </tr>
-    <tr>
-      <td>Mountain Dew</td>
-      <td>500ml</td>
-      <td>70</td>
-      <td>$2.50</td>
-      <td>
-        <button class="view-btn">View</button>
-        <button class="edit-btn">Edit</button>
-      </td>
-    </tr>
-    <tr>
-      <td>Mirinda</td>
-      <td>1 liter</td>
-      <td>30</td>
-      <td>$3.00</td>
-      <td>
-        <button class="view-btn">View</button>
-        <button class="edit-btn">Edit</button>
-      </td>
-    </tr>
-  </tbody>
-</table>
+  const [products, setProducts] = useState([]);
 
-        </div>
-    );
+  useEffect(() => {
+    fetch('http://localhost:3000/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.log(error));
+  }, []);
+  
+console.log (products)
+
+  return (
+    <div style={{ height: "600px", overflow: "scroll" }}>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Stock Amount</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.category}</td>
+              <td>{product.stock_amount}</td>
+              <td>${product.price}</td>
+              <td>
+                <button className="view-btn">View</button>
+                <button className="edit-btn">Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+  
 };
 
 export default ProductList;
