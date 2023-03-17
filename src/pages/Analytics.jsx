9a34acadff88fd996014d4b3fe59e0bfb5c6dd0f
codertinie <martin.munyao@moringaschool.com>
2023-03-17 18:49:47 +0300

    ✨:Added a method to update stock after sell

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -6,87 +6,101 @@ const Analytics = () => {
   const [quantity, setQuantity] = useState(1);
   const [stockAmount, setStockAmount] = useState(0);
 
-  const [products, setProducts] = useState([])
-  const [productName, setProductName] = useState('');
-  const [productId, setProductId] = useState('');
+  const [products, setProducts] = useState([]);
+  const [productName, setProductName] = useState("");
+  const [productId, setProductId] = useState("");
   const [productNames, setProductNames] = useState([]);
 
   useEffect(() => {
-    // Fetch the current stock amount of the product from the server
+    // Fetch the current stock amount and price of the product from the server
     if (productId) {
       fetch(`http://127.0.0.1:3000/products/${productId}`)
-        .then(response => response.json())
-        .then(data => setStockAmount(data.stock_amount))
-        .catch(error => console.error(error));
+        .then((response) => response.json())
+        .then((data) => {
+          setStockAmount(data.stock_amount);
+          setPrice(data.price);
+        })
+        .catch((error) => console.error(error));
     }
   }, [productId]);
 
   useEffect(() => {
     // Fetch the list of product names and IDs from the server
-    fetch('http://127.0.0.1:3000/products')
-      .then(response => response.json())
-      .then(data => {
-        setProducts(data)
-        setProductNames(data.map(product => product.name));
+    fetch("http://127.0.0.1:3000/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        setProductNames(data.map((product) => product.name));
         setProductId(data[0].id); // Set the default product ID to the first product
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
   }, []);
-
-  // console.log( products)
-  // console.log(productName)
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    // Calculate the total price based on the price and quantity
+    const totalPrice = price * quantity;
+
     // Send a request to the server to update the stock amount
     fetch(`http://127.0.0.1:3000/products/${productId}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ quantity })
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ stock_amount: stockAmount - quantity }),
     })
-      .then(response => response.json())
-      .then(data => setStockAmount(data.stock_amount))
-      .catch(error => console.error(error));
-  }
-
+      .then((response) => response.json())
+      .then((data) => {
+        setStockAmount(data.stock_amount);
+      })
+      .catch((error) => console.error(error))
+  };
 
   const handleProductNameChange = (event) => {
     const selectedProductName = event.target.value;
     setProductName(selectedProductName);
-    // Find the product ID for the selected product name
-    const selectedProduct = products.find(product => product.name === selectedProductName);
+    // Find the product ID and price for the selected product name
+    const selectedProduct = products.find(
+      (product) => product.name === selectedProductName
+    );
     if (selectedProduct) {
       setProductId(selectedProduct.id);
+      setPrice(selectedProduct.price);
     }
-    
-  }
-
-  
+  };
+  console.log(products);
   return (
-    <div>
+
+    <div className="analytics">
+      
       <form onSubmit={handleSubmit}>
         <label>
           Product:
           <select value={productName} onChange={handleProductNameChange}>
-            <option value="">-- Select a product --</option>
-            {productNames.map(name => (
-              <option key={name.id} value={name}>{name}</option>
+            <option value="">{}</option>
+            {productNames.map((name) => (
+              <option key={name.id} value={name}>
+                {name}
+              </option>
             ))}
           </select>
         </label>
         <br />
         <label>
           Price:
-          <input type="number" value={price} onChange={(event) => setPrice(event.target.value)} />
+          <input type="number" value={price} readOnly />
         </label>
         <br />
         <label>
           Quantity:
-          <input type="number" value={quantity} onChange={(event) => setQuantity(event.target.value)} />
+          <input
+            type="number"
+            value={quantity}
+            onChange={(event) => setQuantity(event.target.value)}
+          />
         </label>
         <br />
         <button type="submit">Sell</button>
+        <p>Total price: {price * quantity}</p>
         <p>Stock amount: {stockAmount}</p>
       </form>
     </div>
@@ -94,3 +108,5 @@ const Analytics = () => {
 };
 
 export default Analytics;
+
+
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../styles/Dashboard.css";
+import Analytics from "./Analytics";
 
 const Dashboard = () => {
   const [totalProducts, setTotalProducts] = useState(0);
@@ -17,36 +18,86 @@ const Dashboard = () => {
       .catch((error) => console.error(error));
   }, []);
 
+  // const [sales, setSales] = useState([]);
+
+  // const addSale = (productName, quantity, amount) => {
+  //   const newSale = {
+  //     productName,
+  //     quantity,
+  //     amount
+  //   };
+  //   setSales([...sales, newSale]);
+  // };
+
   return (
-    <div class="admin-content mx-auto">
-  <h1 class="page-header">Welcome to Dashboard</h1>
-  <div class="w-75">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 col-md-4">
-          <div class="dash-summary-cell">
-            <p><strong>Total Products</strong></p>
-            <p>{totalProducts}</p>
-          </div>
-        </div>
-        <div class="col-12 col-md-4">
-          <div class="dash-summary-cell">
-            <p><strong>Total Invoices</strong></p>
-            <p>3400</p>
-          </div>
-        </div>
-        <div class="col-12 col-md-4">
-          <div class="dash-summary-cell">
-            <p><strong>Total Sell Today</strong></p>
-            <p>123500</p>
+    <>
+      <div class="admin-content mx-auto">
+        {/* <h1 class="page-header">Welcome to Dashboard</h1> */}
+        <div class="w-75">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <div class="dash-summary-cell">
+                  <p>
+                    <strong>Total Products</strong>
+                  </p>
+                  <p>{totalProducts}</p>
+                </div>
+              </div>
+              <div class="col-12 col-md-4">
+                <div class="dash-summary-cell">
+                  <p>
+                    <strong>Total Invoices</strong>
+                  </p>
+                  <p>3400</p>
+                </div>
+              </div>
+              <div class="col-12 col-md-4">
+                <div class="dash-summary-cell">
+                  <p>
+                    <strong>Total Sell Today</strong>
+                  </p>
+                  <p>123500</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
 
 
+      <h1>Sales Dashboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Quantity</th>
+            <th>Amount</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        {/* <tbody>
+          {sales.map((sale, index) => (
+            <tr key={index}>
+              <td>{sale.productName}</td>
+              <td>{sale.quantity}</td>
+              <td>{sale.amount}</td>
+            </tr>
+          ))}
+        </tbody> */}
+      </table>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
   );
 };
 
