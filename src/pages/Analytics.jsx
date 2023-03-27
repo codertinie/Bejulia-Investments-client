33f9b4d3b36d1142aa19d5435e975394c91d9f4d
codertinie <martin.munyao@moringaschool.com>
2023-03-27 14:13:11 +0300

    🚑️:Improved ux foe selling product

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -5,6 +5,7 @@ const Analytics = () => {
   const [price, setPrice] = useState(0);
   const [quantity, setQuantity] = useState(1);
   const [stockAmount, setStockAmount] = useState(0);
+  const [quantityError, setQuantityError] = useState("");
 
   const [products, setProducts] = useState([]);
   const [productName, setProductName] = useState("");
@@ -36,8 +37,6 @@ const Analytics = () => {
       .catch((error) => console.error(error));
   }, []);
 
-  
-
   const handleProductNameChange = (event) => {
     const selectedProductName = event.target.value;
     setProductName(selectedProductName);
@@ -51,95 +50,115 @@ const Analytics = () => {
     }
   };
 
+  const validateQuantity = (value) => {
+    if (value < 1) {
+      return "Quantity must be at least 1";
+    } else if (value > stockAmount) {
+      return `Quantity cannot exceed the current stock amount of ${stockAmount}`;
+    } else {
+      return "";
+    }
+  };
+
   const handleSellProduct = (event) => {
     event.preventDefault();
 
-    // Calculate the total price based on the price and quantity
-    const totalPrice = price * quantity;
+    // Validate the quantity input
+    const quantityError = validateQuantity(quantity);
+    setQuantityError(quantityError);
 
-    // Send a request to the server to update the stock amount and create a new sale record
-    fetch(`http://127.0.0.1:3000/products/${productId}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ stock_amount: stockAmount - quantity }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setStockAmount(data.stock_amount);
+    if (!quantityError) {
+      // Calculate the total price based on the price and quantity
+      const totalPrice = price * quantity;
+
+      // Send a request to the server to update the stock amount and create a new sale record
+      fetch(`http://127.0.0.1:3000/products/${productId}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ stock_amount: stockAmount - quantity }),
       })
-      .catch((error) => console.error(error));
+        .then((response) => response.json())
+        .then((data) => {
+          setStockAmount(data.stock_amount);
+        })
+        .catch((error) => console.error(error));
 
-      const dateInEAT = new Date(new Date().getTime() + (3 * 60 * 60 * 1000)).toISOString().split("T")[0];
+      const dateInEAT = new Date(new Date().getTime() + 3 * 60 * 60 * 1000)
+        .toISOString()
+        .split("T")[0];
 
-    fetch("http://localhost:3000/sales", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        product_id: productId,
-        quantity: quantity,
-        amount: totalPrice,
-        date: dateInEAT,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          // Notify the user that the sale has been made successfully
-          alert("Sale made successfully!");
-          // Clear the form
-          setProductName("");
-          setProductId("");
-          setPrice(0);
-          setQuantity(0);
-        } else {
-          throw new Error("Failed to create sale");
-        }
+      fetch("http://localhost:3000/sales", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          product_id: productId,
+          quantity: quantity,
+          amount: totalPrice,
+          date: dateInEAT,
+        }),
       })
-      .catch((error) => console.error(error));
+        .then((response) => {
+          if (response.ok) {
+            // Notify the user that the sale has been made successfully
+            alert("Sale made successfully!");
+            // Clear the form
+            setProductName("");
+            setProductId("");
+            setPrice(0);
+            setQuantity(0);
+          } else {
+            throw new Error("Failed to create sale");
+          }
+        })
+        .catch((error) => console.error(error));
+    }
   };
 
   return (
     <div className="analytics">
       <form className="analytics-form" onSubmit={handleSellProduct}>
         <div className="product">
-        <label>
-          Product:
-          <select value={productName} onChange={handleProductNameChange}>
-            <option value="">--Select Product--</option>
-            {productNames.map((productName, index) => (
-              <option key={products[index].id} value={productName}>
-                {productName}
-              </option>
-            ))}
-          </select>
-        </label>
+          <label>
+            Product:
+            <select value={productName} onChange={handleProductNameChange}>
+              <option value="">--Select Product--</option>
+              {productNames.map((productName, index) => (
+                <option key={products[index].id} value={productName}>
+                  {productName}
+                </option>
+              ))}
+            </select>
+          </label>
         </div>
         <br />
         <div>
-        <label>
-          Price:
-          <input type="number" value={price} readOnly />
-        </label>
+          <label>
+            Price:
+            <input type="number" value={price} readOnly />
+          </label>
         </div>
         <div>
-        <label>
-          Quantity:
-          <input
-            type="number"
-            value={quantity}
-            onChange={(event) => setQuantity(event.target.value)}
-          />
-        </label>
+          <label>
+            Quantity:
+            <input
+              type="number"
+              value={quantity}
+              onChange={(event) => setQuantity(event.target.value)}
+            />
+          </label>
+          <div className="error">{quantityError}</div>
         </div>
+
         <br />
         <br />
         <div className="total-price">
-        <p>Total price: Ksh. {price * quantity}</p>
+          <p>Total price: Ksh. {price * quantity}</p>
         </div>
         <div className="stock-amount">
-        <p> {stockAmount} units remaining</p>
+          <p> {stockAmount} units remaining</p>
         </div>
         <div>
-        <button type="submit">Sell</button>
+          <button type="submit">Sell</button>
         </div>
       </form>
     </div>
